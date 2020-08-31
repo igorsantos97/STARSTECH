1928180406672dbfeac6efe37e3b621e8c487bef
Igor Santos <igornzds@gmail.com>
2020-08-31 16:15:42 -0300

    Ajustar navegação por tabs para acessibilidade

--- js/script.js
+++ js/script.js
@@ -2,22 +2,27 @@ function activeNavTab() {
     const textTabnav = document.querySelectorAll('.js-tabnav-content p');
     const menuTabnav = document.querySelectorAll('.js-tabnav-menu li');
 
-    function activeTabnav(index) {
-        textTabnav.forEach((text, index) => {
-            text.classList.remove('active');
+    if (textTabnav && menuTabnav) {
+        textTabnav[0].classList.add('active');
+        menuTabnav[0].classList.add('active');
 
-            menuTabnav[index].classList.remove('active');
+        function activeTabnav(index) {
+            textTabnav.forEach((text, index) => {
+                text.classList.remove('active');
+
+                menuTabnav[index].classList.remove('active');
+            });
+
+            textTabnav[index].classList.add('active');
+            menuTabnav[index].classList.add('active');
+        }
+
+        menuTabnav.forEach((item, index) => {
+            item.addEventListener('click', function () {
+                activeTabnav(index);
+            });
         });
-
-        textTabnav[index].classList.add('active');
-        menuTabnav[index].classList.add('active');
     }
-
-    menuTabnav.forEach((item, index) => {
-        item.addEventListener('click', function () {
-            activeTabnav(index);
-        });
-    });
 }
 
 
