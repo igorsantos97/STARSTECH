f5b8ef533df4acd5378363c8b5dfe0b52ca0e365
Igor Santos <igornzds@gmail.com>
2020-08-31 16:06:22 -0300

    Finalizar navegação por tabs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 function activeNavTab() {
+    const textTabnav = document.querySelectorAll('.js-tabnav-content p');
+    const menuTabnav = document.querySelectorAll('.js-tabnav-menu li');
 
+    function activeTabnav(index) {
+        textTabnav.forEach((text, index) => {
+            text.classList.remove('active');
+
+            menuTabnav[index].classList.remove('active');
+        });
+
+        textTabnav[index].classList.add('active');
+        menuTabnav[index].classList.add('active');
+    }
+
+    menuTabnav.forEach((item, index) => {
+        item.addEventListener('click', function () {
+            activeTabnav(index);
+        });
+    });
 }
+
+
+activeNavTab();
